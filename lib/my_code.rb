2f7b388fd726317de03cbd96665eecd888eab3de
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-03 21:29:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,14 +10,15 @@ end
 
 def reduce(source_array, start = nil)
   if start
-    num1 = start
+    sum = start
     i = 0 
   else
-    num1 = source_array[0]
+    sum = source_array[0]
     i = 1
   end
   
   while i < source_array.length do
-    yield(num1, source_array[i])
+    sum = yield(sum, source_array[i])
+    i += 1
   end
 end
